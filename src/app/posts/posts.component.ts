be10e1f39091a0b0166b8e544a8b6fc82da0da9d
InkyChew <inky11308@gmail.com>
2025-03-06 14:27:09 +0800

    posts page css

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -2,10 +2,13 @@ import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PostService } from '../services/post.service';
 import { Post } from '../models/post';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-posts',
-  imports: [RouterModule],
+  imports: [RouterModule, MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
@@ -22,5 +25,16 @@ export class PostsComponent {
     this._service.getPosts().subscribe(res => {
       this.posts.set(res);
     });
+  }
+
+  getSummary(html: string, wordLimit: number = 50): string {
+    // Parse the HTML to extract text content
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const textContent = doc.body.textContent || "";
+
+    // Split the text into words and limit the number of words to the given limit
+    const words = textContent.split(/\s+/).join(' '); // Split by any whitespace character
+    return words.substring(0, wordLimit);
   }
 }
